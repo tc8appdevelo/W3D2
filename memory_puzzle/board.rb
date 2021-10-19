83631c5a2c1742e49709801381af1198933d2087
tasangpo <tashi.rayshar@gmail.com>
2021-10-19 13:32:38 -0400

    updated

--- memory_puzzle/board.rb
+++ memory_puzzle/board.rb
@@ -1,10 +1,31 @@
+require_relative "card"
+
 class Board
     def initialize(n)
         @grid = Array.new(n){Array.new(n)}
+    end
 
+    def [](pos)
+        @grid[pos[0]][pos[1]]
+    end
+
+    def []=(pos,val)
+        @grid[pos[0]][pos[1]] = val
     end
 
     def populate
-        
+        alphabet = ("a".."z").to_a
+        alpha_idx = 0
+        @grid.each_with_index do |array, idx_1|
+            array.each_with_index do |card, idx_2|
+                row = rand(@grid.length)
+                col = rand(@grid.length)
+                pos = [row,col]
+                if self[pos] == nil
+                    self[pos] = alphabet[alpha_idx]
+                    alpha_idx += 1
+                end
+            end
+        end
     end
 end
